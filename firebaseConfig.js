47eebf77f7930ba5ece6173907f5f7b951d3a2db
Débora Oliveira <debora.oliveira2138@gmail.com>
2026-05-26 23:58:44 -0300

    Update firebaseConfig to include Auth and Firestore

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,11 +1,11 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// firebaseConfig.js
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "firebase/app";
+
+import { getAuth } from "firebase/auth";
+
+import { getFirestore } from "firebase/firestore";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDCfS8_isqypSowk3C6Bc1BTFekgt1CYqQ",
   authDomain: "meumundo-ab065.firebaseapp.com",
@@ -16,6 +16,11 @@ const firebaseConfig = {
   measurementId: "G-8RQ78JW4V3"
 };
 
-// Initialize Firebase
+// inicia firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// auth
+export const auth = getAuth(app);
+
+// firestore
+export const db = getFirestore(app);
