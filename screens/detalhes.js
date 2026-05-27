62e59f1d4ca6993c53384a39f0a0f4a924105a3d
ana <anabeatriz02062009@gmail.com>
2026-05-27 10:14:00 -0300

    Update detalhes.js

--- screens/detalhes.js
+++ screens/detalhes.js
@@ -1,76 +1,46 @@
-import React from 'react';
+import React from "react";
 
-import {
-  View,
- Text,
-  Image,
-  TouchableOpacity,
-  ScrollView
-} from 'react-native';
+import { View, Text, Image, TouchableOpacity, ScrollView } from "react-native";
 
-import {
-  addDoc,
-  collection
-} from 'firebase/firestore';
-
-import {
-  getAuth
-} from 'firebase/auth';
-
-import { db } from '../firebaseConfig';
+import axios from "axios";
 
 export default function Detalhes({ route, navigation }) {
-
   const { pais } = route.params;
 
   async function favoritar() {
-
     try {
+      await axios.post("http://localhost:3000/favoritos", {
+        nome: pais.name.common,
+        capital: pais.capital?.[0],
+        population: pais.population,
+        bandeira: pais.flags.png,
+      });
 
-      const auth = getAuth();
-
-      const user = auth.currentUser;
-
-      await addDoc(
-        collection(db, 'favoritos'),
-        {
-          uid: user.uid,
-          nome: pais.name.common,
-          capital: pais.capital?.[0],
-          bandeira: pais.flags.png
-        }
-      );
-
-      alert('País favoritado!');
-
+      alert("País adicionado aos favoritos!");
     } catch (error) {
-
       console.log(error);
 
+      alert("Erro ao adicionar país aos favoritos!");
     }
-
   }
 
   return (
-
     <View
       style={{
         flex: 1,
-        backgroundColor: '#f4f6fb'
+        backgroundColor: "#f4f6fb",
       }}
     >
-
       <ScrollView
         showsVerticalScrollIndicator={false}
         contentContainerStyle={{
-          paddingBottom: 40
+          paddingBottom: 40,
         }}
       >
-
         {/* HEADER */}
         <View
           style={{
-            backgroundColor: '#0d5be1',
+            backgroundColor: "#0d5be1",
 
             paddingTop: 60,
             paddingBottom: 25,
@@ -81,112 +51,92 @@ export default function Detalhes({ route, navigation }) {
             borderBottomRightRadius: 30,
           }}
         >
-
           <View
             style={{
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-              alignItems: 'center'
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
             }}
           >
-
             {/* VOLTAR */}
-            <TouchableOpacity
-              onPress={() => navigation.goBack()}
-            >
-
+            <TouchableOpacity onPress={() => navigation.goBack()}>
               <Image
                 source={{
-                  uri: 'https://cdn-icons-png.flaticon.com/512/271/271220.png'
+                  uri: "https://cdn-icons-png.flaticon.com/512/271/271220.png",
                 }}
-
                 style={{
                   width: 24,
                   height: 24,
-                  tintColor: 'white'
+                  tintColor: "white",
                 }}
               />
-
             </TouchableOpacity>
 
             {/* TÍTULO */}
             <Text
               style={{
-                color: 'white',
+                color: "white",
                 fontSize: 22,
-                fontWeight: 'bold'
+                fontWeight: "bold",
               }}
             >
               Detalhes do País
             </Text>
 
             {/* FAVORITO */}
-            <TouchableOpacity>
-
+            <TouchableOpacity onPress={favoritar}>
               <Image
                 source={{
-                  uri: 'https://cdn-icons-png.flaticon.com/512/833/833472.png'
+                  uri: "https://cdn-icons-png.flaticon.com/512/833/833472.png",
                 }}
-
                 style={{
                   width: 24,
                   height: 24,
-                  tintColor: 'white'
+                  tintColor: "white",
                 }}
               />
-
             </TouchableOpacity>
-
           </View>
-
         </View>
 
         {/* CARD */}
         <View
           style={{
-            backgroundColor: 'white',
+            backgroundColor: "white",
 
             margin: 18,
             marginTop: -10,
 
             borderRadius: 28,
 
-            overflow: 'hidden',
+            overflow: "hidden",
 
-            elevation: 5
+            elevation: 5,
           }}
         >
-
-          {/* IMAGEM PAÍS */}
+          {/* IMAGEM */}
           <Image
-
             source={{
-              uri: pais.flags.png
+              uri: pais.flags.png,
             }}
-
             style={{
-              width: '100%',
-              height: 230
+              width: "100%",
+              height: 230,
             }}
-
             resizeMode="cover"
-
           />
 
           {/* CONTEÚDO */}
           <View
             style={{
-              padding: 22
+              padding: 22,
             }}
           >
-
-            {/* BANDEIRA PEQUENA */}
+            {/* BANDEIRA */}
             <Image
-
               source={{
-                uri: pais.flags.png
+                uri: pais.flags.png,
               }}
-
               style={{
                 width: 70,
                 height: 50,
@@ -196,19 +146,18 @@ export default function Detalhes({ route, navigation }) {
                 marginTop: -55,
 
                 borderWidth: 4,
-                borderColor: 'white',
+                borderColor: "white",
 
-                marginBottom: 18
+                marginBottom: 18,
               }}
-
             />
 
             {/* NOME */}
             <Text
               style={{
                 fontSize: 34,
-                fontWeight: 'bold',
-                color: '#111'
+                fontWeight: "bold",
+                color: "#111",
               }}
             >
               {pais.name.common}
@@ -217,144 +166,135 @@ export default function Detalhes({ route, navigation }) {
             {/* NOME OFICIAL */}
             <Text
               style={{
-                color: '#777',
+                color: "#777",
                 fontSize: 16,
 
                 marginTop: 5,
-                marginBottom: 30
+                marginBottom: 30,
               }}
             >
               {pais.name.official}
             </Text>
 
-            {/* INFO */}
+            {/* INFORMAÇÕES */}
             {[
               {
-                icon: 'https://cdn-icons-png.flaticon.com/512/535/535137.png',
-                titulo: 'Capital',
-                valor: pais.capital?.[0]
+                icon: "https://cdn-icons-png.flaticon.com/512/535/535137.png",
+                titulo: "Capital",
+                valor: pais.capital?.[0] || "Não informado",
               },
 
               {
-                icon: 'https://cdn-icons-png.flaticon.com/512/747/747376.png',
-                titulo: 'População',
-                valor: pais.population?.toLocaleString()
+                icon: "https://cdn-icons-png.flaticon.com/512/747/747376.png",
+                titulo: "População",
+                valor: pais.population?.toLocaleString(),
               },
 
               {
-                icon: 'https://cdn-icons-png.flaticon.com/512/3898/3898082.png',
-                titulo: 'Idioma',
+                icon: "https://cdn-icons-png.flaticon.com/512/3898/3898082.png",
+                titulo: "Idioma",
                 valor: pais.languages
-                  ? Object.values(pais.languages).join(', ')
-                  : 'Não informado'
+                  ? Object.values(pais.languages).join(", ")
+                  : "Não informado",
               },
 
               {
-                icon: 'https://cdn-icons-png.flaticon.com/512/2489/2489756.png',
-                titulo: 'Moeda',
+                icon: "https://cdn-icons-png.flaticon.com/512/2489/2489756.png",
+                titulo: "Moeda",
                 valor: pais.currencies
                   ? Object.values(pais.currencies)
-                      .map(item => `${item.name}`)
-                      .join(', ')
-                  : 'Não informado'
+                      .map((item) => item.name)
+                      .join(", ")
+                  : "Não informado",
               },
 
               {
-                icon: 'https://cdn-icons-png.flaticon.com/512/684/684908.png',
-                titulo: 'Região',
-                valor: pais.region
+                icon: "https://cdn-icons-png.flaticon.com/512/684/684908.png",
+                titulo: "Região",
+                valor: pais.region || "Não informado",
               },
 
               {
-                icon: 'https://cdn-icons-png.flaticon.com/512/854/854878.png',
-                titulo: 'Sub-região',
-                valor: pais.subregion || 'Não informado'
+                icon: "https://cdn-icons-png.flaticon.com/512/854/854878.png",
+                titulo: "Sub-região",
+                valor: pais.subregion || "Não informado",
               },
 
               {
-                icon: 'https://cdn-icons-png.flaticon.com/512/854/854866.png',
-                titulo: 'Continente',
-                valor: pais.continents?.[0] || 'Não informado'
+                icon: "https://cdn-icons-png.flaticon.com/512/854/854866.png",
+                titulo: "Continente",
+                valor: pais.continents?.[0] || "Não informado",
               },
 
               {
-                icon: 'https://cdn-icons-png.flaticon.com/512/2972/2972531.png',
-                titulo: 'Fuso horário',
-                valor: pais.timezones?.[0]
-              }
+                icon: "https://cdn-icons-png.flaticon.com/512/2972/2972531.png",
+                titulo: "Fuso horário",
+                valor: pais.timezones?.[0] || "Não informado",
+              },
             ].map((item, index) => (
-
               <View
                 key={index}
                 style={{
-                  flexDirection: 'row',
-                  alignItems: 'center',
+                  flexDirection: "row",
+                  alignItems: "center",
 
-                  justifyContent: 'space-between',
+                  justifyContent: "space-between",
 
-                  marginBottom: 24
+                  marginBottom: 24,
                 }}
               >
-
                 {/* ESQUERDA */}
                 <View
                   style={{
-                    flexDirection: 'row',
-                    alignItems: 'center'
+                    flexDirection: "row",
+                    alignItems: "center",
                   }}
                 >
-
                   <Image
                     source={{
-                      uri: item.icon
+                      uri: item.icon,
                     }}
-
                     style={{
                       width: 22,
                       height: 22,
 
-                      tintColor: '#777',
+                      tintColor: "#777",
 
-                      marginRight: 14
+                      marginRight: 14,
                     }}
                   />
 
                   <Text
                     style={{
                       fontSize: 18,
-                      color: '#222'
+                      color: "#222",
                     }}
                   >
                     {item.titulo}
                   </Text>
-
                 </View>
 
                 {/* DIREITA */}
                 <Text
                   style={{
                     fontSize: 17,
-                    fontWeight: '600',
-                    color: '#222',
+                    fontWeight: "600",
+                    color: "#222",
 
-                    maxWidth: '50%',
-                    textAlign: 'right'
+                    maxWidth: "50%",
+                    textAlign: "right",
                   }}
                 >
                   {item.valor}
                 </Text>
-
               </View>
-
             ))}
 
             {/* BOTÃO */}
             <TouchableOpacity
-
               onPress={favoritar}
-
               style={{
-                backgroundColor: '#0d5be1',
+                backgroundColor: "#0d5be1",
 
                 padding: 18,
 
@@ -362,47 +302,38 @@ export default function Detalhes({ route, navigation }) {
 
                 marginTop: 10,
 
-                flexDirection: 'row',
-                justifyContent: 'center',
-                alignItems: 'center'
+                flexDirection: "row",
+                justifyContent: "center",
+                alignItems: "center",
               }}
             >
-
               <Image
                 source={{
-                  uri: 'https://cdn-icons-png.flaticon.com/512/833/833472.png'
+                  uri: "https://cdn-icons-png.flaticon.com/512/833/833472.png",
                 }}
-
                 style={{
                   width: 20,
                   height: 20,
 
-                  tintColor: 'white',
+                  tintColor: "white",
 
-                  marginRight: 10
+                  marginRight: 10,
                 }}
               />
 
               <Text
                 style={{
-                  color: 'white',
+                  color: "white",
                   fontSize: 17,
-                  fontWeight: 'bold'
+                  fontWeight: "bold",
                 }}
               >
                 Adicionar aos Favoritos
               </Text>
-
             </TouchableOpacity>
-
           </View>
-
         </View>
-
       </ScrollView>
-
     </View>
-
   );
-
 }
